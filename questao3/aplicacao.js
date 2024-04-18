2b4a3f96b1e1fb2b487dd5f515e3b0d1295913d6
leonardoCottet <126694203+leonardoCottet@users.noreply.github.com>
2024-04-18 12:17:40 -0300

    Update aplicacao.js

---
+++ questao3/aplicacao.js
@@ -0,0 +1,6 @@
+function submitForm() {
+    var numero = document.getElementById("numero").value;
+    var mensagem = document.getElementById("mensagem");
+    message.style.display = "block";
+    message.innerHTML = "Você inseriu o número " + number;
+    return false;
