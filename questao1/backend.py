2b0e65088874bd3ac8fb96c77bd682414a29f368
Leonardo Cottet Didiano <22010981@fecap.br>
2024-04-18 11:15:49 -0300

    questão 1

--- questao1/backend.py
+++ questao1/backend.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List
 
@@ -6,6 +6,8 @@ app = FastAPI()
 
 class Item(BaseModel):
     description: str
+#tarefa 3
+    assigned_to: str 
 
 # Lista temporária para armazenar as tarefas
 tasks: List[Item] = []
@@ -18,6 +20,22 @@ async def add_item(item: Item):
 @app.get("/items/", response_model=List[Item])
 async def get_items():
     return tasks
+#tarefa 1
+@app.delete("/items/{item_id}")
+async def delete_item(item_id: int):
+    try:
+        del tasks[item_id]
+        return {"message": f"Item with ID {item_id} deleted successfully"}
+    except IndexError:
+        raise HTTPException(status_code=404, detail=f"Item with ID {item_id} not found")
+#tarefa 2
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, updated_item: Item):
+    try:
+        tasks[item_id] = updated_item
+        return {"message": f"Item with ID {item_id} updated successfully"}
+    except IndexError:
+        raise HTTPException(status_code=404, detail=f"Item with ID {item_id} not found")
 
 if __name__ == "__main__":
     import uvicorn
