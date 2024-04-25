5e0ee85da6487a24bc99228d517e9d618043cdd9
Vitorio Waldemarin Bearari <22010892@fecap.br>
2024-04-25 12:47:36 -0300

    sub vitorio 2

--- questao1/frontend.py
+++ questao1/frontend.py
@@ -1,39 +1,176 @@
 import flet as ft
-import requests
 
-def main(page: ft.Page):
-    page.title = "To-Do List App"
-    page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
-    # Elementos da UI
-    input_text = ft.TextField(hint_text="Add a new task", width=300, autofocus=True)
-    submit_button = ft.ElevatedButton(text="Add", on_click=lambda _: add_task(page, input_text))
-    task_list = ft.ListView(expand=True)
-    
-    # Função para adicionar tarefa
-    def add_task(page, input_text):
-        description = input_text.value.strip()
-        if description:
-            response = requests.post("http://localhost:8080/items/", json={"description": description})
-            if response.status_code == 200:
-                input_text.value = ""
-                update_tasks(page, task_list)
-            else:
-                page.snackbar_text = "Failed to add item"
-        page.update()
+class Task(ft.UserControl):
+    def __init__(self, task_name, task_status_change, task_delete):
+        super().__init__()
+        self.completed = False
+        self.task_name = task_name
+        self.task_status_change = task_status_change
+        self.task_delete = task_delete
 
-    # Função para atualizar a lista de tarefas
-    def update_tasks(page, task_list):
-        response = requests.get("http://localhost:8080/items/")
-        task_list.controls.clear()
-        if response.status_code == 200:
-            items = response.json()
-            for item in items:
-                task_list.controls.append(ft.ListTile(title=ft.Text(item['description'])))
-        page.update()
+    def build(self):
+        self.display_task = ft.Checkbox(
+            value=False, label=self.task_name, on_change=self.status_changed
+        )
+        self.edit_name = ft.TextField(expand=1)
 
-    # Adicionar elementos à página
-    page.add(input_text, submit_button, task_list)
-    update_tasks(page, task_list)
+        self.display_view = ft.Row(
+            alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+            vertical_alignment=ft.CrossAxisAlignment.CENTER,
+            controls=[
+                self.display_task,
+                ft.Row(
+                    spacing=0,
+                    controls=[
+                        ft.IconButton(
+                            icon=ft.icons.CREATE_OUTLINED,
+                            tooltip="Editar",
+                            on_click=self.edit_clicked,
+                        ),
+                        ft.IconButton(
+                            ft.icons.DELETE_OUTLINE,
+                            tooltip="Deletar",
+                            on_click=self.delete_clicked,
+                        ),
+                    ],
+                ),
+            ],
+        )
 
-ft.app(target=main, port=8000)
+        self.edit_view = ft.Row(
+            visible=False,
+            alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+            vertical_alignment=ft.CrossAxisAlignment.CENTER,
+            controls=[
+                self.edit_name,
+                ft.IconButton(
+                    icon=ft.icons.DONE_OUTLINE_OUTLINED,
+                    icon_color=ft.colors.GREEN,
+                    tooltip="Confirmar",
+                    on_click=self.save_clicked,
+                ),
+            ],
+        )
+        return ft.Column(controls=[self.display_view, self.edit_view])
+
+    async def edit_clicked(self, e):
+        self.edit_name.value = self.display_task.label
+        self.display_view.visible = False
+        self.edit_view.visible = True
+        await self.update_async()
+
+    async def save_clicked(self, e):
+        self.display_task.label = self.edit_name.value
+        self.display_view.visible = True
+        self.edit_view.visible = False
+        await self.update_async()
+
+    async def status_changed(self, e):
+        self.completed = self.display_task.value
+        await self.task_status_change(self)
+
+    async def delete_clicked(self, e):
+        await self.task_delete(self)
+
+
+class TodoApp(ft.UserControl):
+    def build(self):
+        self.new_task = ft.TextField(
+            hint_text="Oque precisa ser feito?", on_submit=self.add_clicked, expand=True
+        )
+        self.tasks = ft.Column()
+
+        self.filter = ft.Tabs(
+            scrollable=False,
+            selected_index=0,
+            on_change=self.tabs_changed,
+            tabs=[ft.Tab(text="todas"), ft.Tab(text="ativas"), ft.Tab(text="completas")],
+        )
+
+        self.items_left = ft.Text("0 items left")
+
+        # application's root control (i.e. "view") containing all other controls
+        return ft.Column(
+            width=600,
+            controls=[
+                ft.Row(
+                    [ft.Text(value="Lembretes", style=ft.TextThemeStyle.HEADLINE_MEDIUM)],
+                    alignment=ft.MainAxisAlignment.CENTER,
+                ),
+                ft.Row(
+                    controls=[
+                        self.new_task,
+                        ft.FloatingActionButton(
+                            icon=ft.icons.ADD, on_click=self.add_clicked
+                        ),
+                    ],
+                ),
+                ft.Column(
+                    spacing=25,
+                    controls=[
+                        self.filter,
+                        self.tasks,
+                        ft.Row(
+                            alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+                            vertical_alignment=ft.CrossAxisAlignment.CENTER,
+                            controls=[
+                                self.items_left,
+                                ft.OutlinedButton(
+                                    text="Limpar Completas", on_click=self.clear_clicked
+                                ),
+                            ],
+                        ),
+                    ],
+                ),
+            ],
+        )
+
+    async def add_clicked(self, e):
+        if self.new_task.value:
+            task = Task(self.new_task.value, self.task_status_change, self.task_delete)
+            self.tasks.controls.append(task)
+            self.new_task.value = ""
+            await self.new_task.focus_async()
+            await self.update_async()
+
+    async def task_status_change(self, task):
+        await self.update_async()
+
+    async def task_delete(self, task):
+        self.tasks.controls.remove(task)
+        await self.update_async()
+
+    async def tabs_changed(self, e):
+        await self.update_async()
+
+    async def clear_clicked(self, e):
+        for task in self.tasks.controls[:]:
+            if task.completed:
+                await self.task_delete(task)
+
+    async def update_async(self):
+        status = self.filter.tabs[self.filter.selected_index].text
+        count = 0
+        for task in self.tasks.controls:
+            task.visible = (
+                status == "todas"
+                or (status == "ativas" and task.completed == False)
+                or (status == "completas" and task.completed)
+            )
+            if not task.completed:
+                count += 1
+        self.items_left.value = f"{count} items ativos"
+        await super().update_async()
+
+
+async def main(page: ft.Page):
+    page.title = "Agenda de Lembretes"
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.scroll = ft.ScrollMode.ADAPTIVE
+
+    # create app control and add it to the page
+    await page.add_async(TodoApp())
+
+
+ft.app(main)
